88b6d125050ce7d04e5391ee15451b211caebc11
Rachel Carlsen <lrcarlsen@gmail.com>
2018-04-29 23:43:43 -0700

    fixed toggle

--- AAS.js
+++ AAS.js
@@ -5,10 +5,12 @@ $( function() {
   $(".teams").prepend("<div class='hider'>-</div>");
   $( ".teams" ).draggable({ snap: false });
   //Slide up and down on click
-  $(".hider").on('click', function () {
+  function toggle() {
       $(this).toggleClass('inactive');
       var newText = $(this).text()=='+'?'-':'+';
       $(this).text(newText);
-      $(this).siblings('section').slideToggle("slow");
-  });
+      $(this).siblings('section').toggle();
+  }
+  $(".hider").on('click', toggle);
+  $(".hider").each(toggle);
 } );
